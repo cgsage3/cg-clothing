b0512694f02aa9d382bc79476b9619036ae5aad8
Cesar Granda <info@cgranda.com>
2023-09-11 16:03:06 -0400

    created sing up component, the form input component, button component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
 	signInWithRedirect, 
 	signInWithPopup, 
 	GoogleAuthProvider,
+	createUserWithEmailAndPassword
 
 } from 'firebase/auth';
 import {
@@ -39,7 +40,12 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+	userAuth, 
+	additionalInformation = {}
+	) => {
+	if(!userAuth) return;
+
 	const userDocRef = doc(db, 'users', userAuth.uid );
 
 	console.log(userDocRef);
@@ -56,13 +62,20 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 			await setDoc(userDocRef, {
 				displayName,
 				email,
-				createdAt
+				createdAt,
+				...additionalInformation
 			});
 		}
 		catch (error){
-			console.log('error crating the user', error.message);
+			console.log('error creating the user', error.message);
 		}
 	}
 
 	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	return await createUserWithEmailAndPassword(auth, email, password)
 }

cf68596501b07630e1ebb9201e0b87d052cbc367
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-09-21 17:09:55 -0400

    on lesson 143

--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
 import {UserContext} from '../../contexts/user.context';
 import { 
@@ -11,7 +11,7 @@ import {
 	signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
 
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 
 const defaultFormFields = {
@@ -60,7 +60,7 @@ const SignInForm = () => {
 	};
 
 	return (
-		<div className="sign-up-container">
+		<SignInContainer>
 			<h2>Already have an account?</h2>
 			<span>Sign in with your email and password</span>
 			<form onSubmit={handleSubmit}>
@@ -81,12 +81,12 @@ const SignInForm = () => {
 					name="password"
 					value={password}
 				/>
-				<div className="buttons-container">
+				<ButtonsContainer>
 					<Button type="submit">Sign In</Button>
-					<Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign In</Button>
-				</div>
+					<Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google sign In</Button>
+				</ButtonsContainer>
 			</form>
-		</div>
+		</SignInContainer>
 	)
 }
 
--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -10,7 +10,7 @@ import {
 import { UserContext } from '../../contexts/user.context';
 
 
-import './sign-up-form.styles.scss';
+import { SignUpContainer } from './sign-up-form.styles';
 
 
 const defaultFormFields = {
@@ -59,7 +59,7 @@ const SignUpForm = () => {
 	};
 
 	return (
-		<div className="sign-up-container">
+		<SignUpContainer>
 			<h2>Don't have an account?</h2>
 			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
@@ -105,7 +105,7 @@ const SignUpForm = () => {
 					Sign Up
 				</Button>
 			</form>
-		</div>
+		</SignUpContainer>
 	)
 }
 
